c6b642943633dcab01c10947fb3d9feacc38f4db
Sohailaibrahime66 <124284142+Sohailaibrahime66@users.noreply.github.com>
2025-04-19 18:29:33 +0200

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('parking.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
